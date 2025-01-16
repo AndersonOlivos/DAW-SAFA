4ca328784165d3d98b11faca27bb20ad85582c01
Anderson <anderolivosg@gmail.com>
2025-01-16 16:29:44 +0100

    Practica 2

---
+++ 1° DAW/ENTORNO DE DESARROLLO/2TRIMESTRE/Practica2/src/FechaImpl.java
@@ -0,0 +1,34 @@
+public class FechaImpl implements Fecha{
+    private Integer dia, mes, anio;
+
+    //CONSTRUCTOR
+
+    public FechaImpl(Integer dia, Integer mes, Integer anio) {
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
+
+    //DIA: consultable
+
+    public Integer getDia() {
+        return dia;
+    }
+
+    //MES: consultable
+
+    public Integer getMes() {
+        return mes;
+    }
+
+    //ANIO: consultable
+
+    public Integer getAnio() {
+        return anio;
+    }
+
+    public String toString() {
+        return dia + "/" + mes + "/" + anio;
+    }
+
+}
