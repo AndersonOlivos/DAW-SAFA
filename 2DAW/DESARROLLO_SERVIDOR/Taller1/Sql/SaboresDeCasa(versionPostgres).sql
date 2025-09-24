ac9d593edf90d5510f5cabece657d5311bb879df
Anderson Olivos <anderolivosg@gmail.com>
2025-09-24 17:58:18 +0200

    Ejercicios

--- 2DAW/DESARROLLO_SERVIDOR/Taller1/Sql/SaboresDeCasa(versionPostgres).sql
+++ 2DAW/DESARROLLO_SERVIDOR/Taller1/Sql/SaboresDeCasa(versionPostgres).sql
@@ -1,24 +1,39 @@
 create table mesa (
     id serial primary key,
-    numero int,
+    numero int, /* de 101 a 120 se refiere a interior y de 201 a 220 se refiere a terraza */
     disponible bool default true,
-    num_com_max int
+    num_comesales_max int
+);
+
+create table cliente (
+	id serial primary key,
+	nombre varchar(50),
+	apellidos varchar(120),
+	telefono int,
+	correo varchar(150),
+	contrasena varchar(600)
 );
 
 create table pedido (
     id serial primary key,
-    cliente varchar(40) null,
+    id_cliente int null,
     id_mesa int,
+    num_comensales int,
+    precio_total numeric(10,2) null,
+    estado int default 0, /*PENDIENTE, COMPLETADO, CANCELADO*/
+    constraint fk_pedido_cliente foreign key (id_cliente) references cliente(id),
     constraint fk_pedido_mesa foreign key (id_mesa) references mesa(id)
 );
 
 create table producto (
     id serial primary key,
     nombre varchar(50),
-    descripcion text,
-    precio numeric(10, 2)
+    ingredientes text,
+    precio numeric(10, 2),
+    tipo int default 0, /*BEBIDA CALIENTE, REFRESCO, COMIDA, ETC*/
+    subtipo int default 0 /*NULL, TOSTADA, DULCE, ETC*/
 );
-
+	
 create table lpedido (
     id serial primary key,
     id_pedido int,
@@ -26,13 +41,6 @@ create table lpedido (
     cantidad int,
     constraint fk_lpedido_pedido foreign key (id_pedido) references pedido(id),
 	constraint fk_lpedido_producto foreign key (id_producto) references producto(id)
-    );
-
-CREATE TABLE cliente (
-	id serial PRIMARY KEY,
-	nombre varchar(50),
-	apellidos varchar(120),
-	telefono int,
-	correo varchar(150),
-	contrasena varchar(600)
 );
+
+
