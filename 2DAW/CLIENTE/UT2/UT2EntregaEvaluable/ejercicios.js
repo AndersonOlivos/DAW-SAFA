c449172a591910e0afdb4e1dd0030168312ff291
Anderson Olivos <anderolivosg@gmail.com>
2025-10-14 19:15:25 +0200

    Nombre cambiado y fichero JS comentado

--- 2DAW/CLIENTE/UT2/UT2EntregaEvaluable/ejercicios.js
+++ 2DAW/CLIENTE/UT2/UT2EntregaEvaluable/ejercicios.js
@@ -5,6 +5,20 @@ Curso: 2025/2026
 Módulo del autor: Desarrollo web en entorno cliente
 */
 
+//EJERCICIO 1
+/* 
+Se requiere que el usuario introduzca una frase de, al menos, cinco palabras mediante un
+prompt. De no ser así, mostrará un mensaje y volverá a pedir de nuevo la frase.
+La siguiente información se mostrará por log de consola, modificando el estilo predefinido
+del mismo:
+1) La aplicación Javascript ha de contar el número de caracteres de la frase que no
+sean espacios.
+2) De igual modo, por un lado debe sustituir en la frase original todas las vocales por
+asteriscos.
+3) Se sustituirá cada elemento par (ignorando los espacios para el conteo) de la frase
+por un guion bajo (“_”).
+*/
+
 const ejercicio1 = () => {
   const input = document.getElementById("inpEjercicio1");
   const aviso = document.getElementById("aviso1");
@@ -65,6 +79,20 @@ const ejercicio1 = () => {
   }
 };
 
+//EJERCICIO 2
+/* 
+Se requiere al usuario que inserte 7 palabras variadas, comenzando algunas por
+mayúsculas y otras no (y algunas de ellas de 4 letras o menos).
+Se mostrarán por pantalla los nombres ordenados alfabéticamente (sin distinción entre
+mayúsculas y minúsculas, sin considerar aquellos de 4 o menos caracteres.
+A continuación, se le pedirá al usuario que introduzca una palabra para ver si está en dicho
+listado.
+Si la palabra está (ignorando mayúsculas y minúsculas), se mostrará un mensaje de tipo
+info por la consola indicando esto.
+Si la palabra no está, se añadirá al listado previo y este volverá a mostrarse por pantalla, de
+nuevo ordenado.
+*/
+
 const ejercicio2 = () => {
   const palabrasArray = document
     .getElementById("inp1Ejercicio2")
@@ -76,13 +104,13 @@ const ejercicio2 = () => {
     }
   });
   document.getElementById("aviso2").innerHTML = palabras.sort((a, b) =>
-    a.localeCompare(b, "es", { ignorePunctuation: true })
+    a.localeCompare(b, "es", { sensitivity: "base" })
   );
   const palabraDentro = document.getElementById("inp2Ejercicio2").value;
   let coincidencia = 0;
   palabras.forEach((palabra) => {
     if (
-      palabra.localeCompare(palabraDentro, "es", { ignorePunctuation: true }) ==
+      palabra.localeCompare(palabraDentro, "es", { sensitivity: "base" }) ==
       0
     ) {
       coincidencia++;
@@ -91,12 +119,14 @@ const ejercicio2 = () => {
   if (coincidencia == 0) {
     palabras.push(palabraDentro);
     document.getElementById("aviso2").innerHTML = palabras.sort((a, b) =>
-      a.localeCompare(b, "es", { ignorePunctuation: true })
+      a.localeCompare(b, "es", { sensitivity: "base" })
     );
   } else {
     console.info("Esta palabra ya está dentro.");
   }
 };
+
+//LISTA DE OBJETOS PARA EJERCICIO 3
 
 let alumnos = [
   {
@@ -116,6 +146,22 @@ let alumnos = [
     nota: 5.5,
   },
 ];
+
+//EJERCICIO 3
+/**
+ * Vamos a guardar información, de partida, de 4 alumnos y las notas que tienen estos (entre 0
+y 10, permitiendo hasta 2 decimales).
+Esta información se mostrará en pantalla.
+Pide al usuario que introduzca el nombre de un estudiante.
+Si el estudiante está entre los registrados, recuperará la nota del mismo y la mostrará por
+pantalla.
+Si el estudiante no está registrado: se mostrará un mensaje indicando que no estaba
+registrado, pero que se dará de alta con la nota que se indique (se le pedirá a continuación).
+Esta nota deberá ser un número entre 0 y 10 igualmente (si no, deberá volver a pedirse).
+Una vez hecho esto, estuviese el alumno ya o no, se mostrará la nota media de la clase por
+consola en un mensaje de tipo INFO. También se mostrará el nombre de todos los alumnos
+de la clase, ordenados alfabéticamente.
+ */
 
 const ejercicio3 = () => {
   const inpNombre = document.getElementById("inp1Ejercicio3").value;
@@ -177,10 +223,12 @@ const ejercicio3 = () => {
   console.info("NOTA MEDIA: " + notaMedia.toFixed(2));
   console.info(
     alumnosNombreOrdenado.sort((a, b) =>
-      a.localeCompare(b, "es", { ignorePunctuation: true })
+      a.localeCompare(b, "es", { sensitivity: "base" })
     )
   );
 };
+
+//FUNCION PARA MOSTRAR/ACTUALIZAR LA LISTA DE ALUMNOS
 
 const mostrar_alumnos = () => {
   const tabla = document.getElementById("body-alumnos");
@@ -198,6 +246,20 @@ const mostrar_alumnos = () => {
 };
 
 mostrar_alumnos();
+
+//EJERCICIO 4
+/**
+ * Se dispone de 2 conjuntos numéricos: el de los múltiplos de 2 y el de los múltiplos de 3,
+ambos conteniendo los respectivos valores desde 0 hasta 30.
+Se pide que se muestre por pantalla un mensaje “Mira el log para ver el resultado” y que, se
+muestre cada uno de los siguientes resultados en una líneas de log diferente (cambiando el
+estilo para cada una de ellas):
+1) Resultado de la unión de ambos conjuntos
+2) Resultado de la intersección de ambos conjuntos
+3) Resultado de la diferencia del primero menos el segundo
+4) Resultado de la diferencia del segundo menos el primero
+5) Resultado de la exclusión de los elementos que pertenecen a ambos conjuntos
+ */
 
 const ejercicio4 = () => {
   let multiplosDos = new Set();
@@ -251,6 +313,14 @@ const ejercicio4 = () => {
   console.log(`%c${stringExclusion}`, "color: green; font-weight: bold;");
 };
 
+//EJERCICIO 5
+/**
+ * Pide al usuario que dé una serie de números separados por comas. Si algún valor no es
+numérico se volverá a pedir que introduzca valores.
+Una vez introducidos correctamente, se mostrarán por pantalla aquellos valores que solo
+aparecen una vez y la media de dichos valores
+ */
+
 const ejercicio5 = () => {
   const numeros = document.getElementById("inp1Ejercicio5").value;
   const aviso = document.getElementById("aviso5");
@@ -287,6 +357,16 @@ const ejercicio5 = () => {
   }
 };
 
+//EJERCICIO 6
+/**
+ * Crea una función que verifique si una frase o palabra que se introduzca por parte del
+usuario es un palíndromo (se lee igual de izquierda a derecha que de derecha a izquierda).
+Para esta comparación solo se tendrán en cuenta los caracteres alfabéticos, teniendo en
+cuenta que las mayúsculas y las minúsculas se consideran la misma letra, así como
+caracteres acentuados (“a” es lo mismo que “Á” para el palíndromo), pero “n” y “Ñ” son
+diferentes.
+ */
+
 const ejercicio6 = () => {
   const texto = document.getElementById("inp1Ejercicio6").value;
   const aviso = document.getElementById("aviso6");
@@ -305,13 +385,13 @@ const ejercicio6 = () => {
     textoSinEspacios.localeCompare(textoInv, "es", { sensitivity: "base" }) ===
     0
   ) {
-    aviso.innerText = "SON PALINDROMOS";
+    aviso.innerText = "Es palíndromo";
   } else {
-    aviso.innerText = "NO SON PALINDROMOS";
+    aviso.innerText = "No es palíndromo";
   }
 };
 
-/* EJERCICIO 7 */
+//LISTA DE OBJETOS DE TAREA PARA EJERCICIO 7
 
 let tareas = [
   {
@@ -320,7 +400,21 @@ let tareas = [
   },
 ];
 
+//VARIABLE PARA SABER EN QUÉ VENTANA SE ENCUENTRA EL USUARIO
+
 let estadoVentana = "Todas";
+
+//EJERCICIO 7
+/**
+ * Se requiere de un programa que gestione una lista de tareas.
+El programa debe permitir agregar tarea, completar tarea (marcar estado como
+completada), eliminar tarea, mostrar lista (mostrar todas las tareas con su estado) y mostrar
+pendientes (mostrar solo aquellas no completadas).
+Se recomienda, en este caso, un botón para cada funcionalidad (al contrario que en el resto
+de ejercicios), aunque también puede hacerse indicando al usuario que indique la acción a
+realizar(1, 2, 3…).
+NOTA: No puede haber dos tareas iguales.
+ */
 
 const ejercicio7 = () => {
   const tareaNombre = document.getElementById("inp1Ejercicio7").value;
@@ -349,6 +443,8 @@ const ejercicio7 = () => {
 
 };
 
+//FUNCION PARA MOSTRAR/ACTUALIZAR LA VENTANA
+
 const actualizar_ventana = (estado) => {
   const botonTodas = document.getElementById("boton-todas");
   const botonCompletadas = document.getElementById("boton-completadas");
@@ -365,7 +461,6 @@ const actualizar_ventana = (estado) => {
   } else {
     if (estado == "Completadas") {
       botonCompletadas.classList.add("boton-activo");
-      console.log(botonCompletadas);
       estadoVentana = estado;
       mostrar_completadas_tareas();
     } else {
@@ -375,6 +470,8 @@ const actualizar_ventana = (estado) => {
     }
   }
 };
+
+//FUNCION QUE MUESTRA TODAS LAS TAREAS
 
 const mostrar_todas_tareas = () => {
   const divTareas = document.getElementById("tareas");
@@ -423,6 +520,8 @@ const mostrar_todas_tareas = () => {
   divTareas.innerHTML = contenido;
 };
 
+//FUNCION QUE MUESTRA LAS TAREAS COMPLETADAS FILTRADAS
+
 const mostrar_completadas_tareas = () => {
   const divTareas = document.getElementById("tareas");
   divTareas.innerHTML = "";
@@ -451,6 +550,8 @@ const mostrar_completadas_tareas = () => {
     }
   }
 };
+
+//FUNCION QUE MUESTRA LAS TAREAS PENDIENTES FILTRADAS
 
 const mostrar_pendientes_tareas = () => {
   const divTareas = document.getElementById("tareas");
@@ -484,10 +585,14 @@ const mostrar_pendientes_tareas = () => {
   }
 };
 
+//FUNCION PARA ELIMINAR UNA TAREA
+
 const eliminar = (id) => {
   tareas.pop(id);
   actualizar_ventana(estadoVentana);
 };
+
+//FUNCION PARA CAMBIAR EL ESTADO DE UNA TAREA A COMPLETADA
 
 const completado = (id) => {
   tareas[id].estado = "completado";
