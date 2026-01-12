cf05190073a4126b2f5e7ea035b96708daf7b2de
anderolivos <anderolivosg@gmail.com>
2026-01-12 19:58:04 +0100

    Ejercicios React

---
+++ 2DAW/CLIENTE/UT5/ejercicios_expresiones_regulares/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Ejercicio1 from "./components/ejercicio1.jsx";
+import Ejercicio2 from "./components/ejercicio2.jsx";
+
+function App() {
+  return (
+    <>
+        <Ejercicio1 />
+        <Ejercicio2 />
+    </>
+  )
+}
+
+export default App
